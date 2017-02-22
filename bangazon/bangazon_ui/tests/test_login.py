525aca7fd4c584dcc419ff0cbf5981d0d422049c
juliakimchung <julhkm@gmail.com>
2017-02-22 16:23:31 -0600

    login test passed

--- bangazon/bangazon_ui/tests/test_login.py
+++ bangazon/bangazon_ui/tests/test_login.py
@@ -3,10 +3,15 @@ import sys
 sys.path.append('../')
 from bangazon_ui.views.login_view import Login
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-class LogInTestCase(TestCase):
+
+class TestCaseLogin(TestCase):
 	def test_login_view_with_username_password(self):
 
-		create_user(username = "jkc", password="jkc123456")
-		self.client.get(reverse('bangazon_ui:login'))
-		return response
+		user_test = User.objects.create_user(username = "jkc", password="jkc123456")
+		user = {'username': "jkc", "password": "jkc12345678"}
+		response = self.client.post(reverse('bangazon_ui:login'), user)
+		
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response.url, "/")
