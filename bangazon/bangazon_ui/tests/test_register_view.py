ceeb6fc0b2497994af2ef0dad8db4fd6994ee984
Ben Marks <onodric@yahoo.com>
2017-02-22 13:03:28 -0600

    WIP test buildout master merge of templates

---
+++ bangazon/bangazon_ui/tests/test_register_view.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from bangazon_ui.models.customer_model import Customer
+from bangazon_ui.views.register_view import Register
+
+class TestRegisterView(TestCase):
+    """
+    """
+
+
+
+    def test_register_view_should_post_correct_information(self)
+        get_context_data()
