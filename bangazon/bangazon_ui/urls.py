f15bdf92ac7ff42a094ba6e8077764422691d4dd
juliakimchung <julhkm@gmail.com>
2017-02-22 15:10:21 -0600

    url change, login.html change

--- bangazon/bangazon_ui/urls.py
+++ bangazon/bangazon_ui/urls.py
@@ -6,6 +6,7 @@ from bangazon_ui.views import login_view
 
 app_name = 'bangazon_ui'
 urlpatterns = [
+
 url(r'^login/$', login_view.Login.as_view(), name='login'),
 url(r'^logout/$', login_view.logout_user, name="logout"),
 ]
