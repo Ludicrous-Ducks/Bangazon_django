cfd77da8fc7480608115b88d1ae6509c01d85af3
Shawn M <forgeforza@outlook.com>
2017-02-23 15:39:36 -0600

    Order Model and Test Order Model files

---
+++ bangazon/bangazon_ui/models/order_model.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append("../")
+from django.db import models
+from django.contrib.auth.models import User
+from bangazon_ui.models.customer_model import Customer
+from bangazon_ui.models.payment_type_model import PaymentType
+from bangazon_ui.models.product_model import Product
+
+
+
+class Order (models.Model):
+    """
+    The Order table pulls information from PaymentType 
+    """
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    completed = models.IntegerField(default = 0)
+    payment_type = models.ForeignKey(PaymentType, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
+
+    class Meta:
+        verbose_name_plural = "Orders"
+
+    def __str__(self):
+        return '{} {}'.format(self.active, self.customer.first_name)
---
+++ bangazon/bangazon_ui/tests/test_order_model.py
@@ -0,0 +1,56 @@
+import sys
+sys.path.append("../")
+from django.test import TestCase
+from django.contrib.auth.models import User
+from bangazon_ui.models.customer_model import Customer
+from bangazon_ui.models.payment_type_model import PaymentType
+from bangazon_ui.models.product_model import Product
+from bangazon_ui.models.order_model import Order
+
+
+class TestOrderModel(TestCase): 
+
+    """
+    This is a test  
+
+    """
+
+def test_order(self):
+
+    user = User.objects.create_user(
+    first_name="Dani",
+    last_name="Dani",
+    username="danidani",
+    password="asdf1234")
+
+    customer = Customer.objects.create(
+    user=user,
+    address="3040 NSS Drive",
+    city="Nashville",
+    state="TN",
+    zip_code="12345",
+    phone="123456789")
+
+    payment = PaymentType.objects.create(
+    customer=customer,
+    payment_type="Visa",
+    account_number="12345678",
+    ccv="111",
+    expiration_date="2017-01-01"
+    )
+
+    product = Product.objects.create(
+    customer=customer,
+    payment_type=payment)
+
+
+
+    order = Order.objects.create(
+    customer=customer,
+    payment_type= payment,
+    product= product,
+    )
+
+    self.assertIsInstance(order.customer, Customer)
+    self.assertIsInstance(order.payment_type, PaymentType)
+    self.assertIsInstance(order.product, Product)
