838e765a5eac90e43043964c054b2e416783c7ac
Dani Adkins <danirenaeadkins12@gmail.com>
2017-02-24 15:59:09 -0600

    added close order view and made some changes to payment type view and model to fix it

--- bangazon/bangazon_ui/tests/test_payment_type.py
+++ bangazon/bangazon_ui/tests/test_payment_type.py
@@ -11,12 +11,8 @@ class TestPaymentType(TestCase):
     This is the test class for the PaymentType model
     Author: Dani Adkins
     """
+    def setUp(self):
 
-    def test_payment_type_returns_defined_fields(self):
-        """
-        This test will test that the payment type information can be targeted and returned
-        Author: Dani Adkins
-        """
         user = User.objects.create_user(
             first_name="Dani",
             last_name="Dani",
@@ -38,13 +34,36 @@ class TestPaymentType(TestCase):
             payment_type="Visa",
             account_number="12345678",
             ccv="111",
-            expiration_date="2017-01-01"
+            expiration_date="01/2017"
             )
+
+    def test_payment_type_returns_defined_fields(self):
+        """
+        This test will test that the payment type information can be targeted and returned
+        Author: Dani Adkins
+        """
+        payment = PaymentType.objects.get(payment_type="Visa")
 
         self.assertIsInstance(payment.customer, Customer)
         self.assertEqual(payment.payment_type, "Visa")
         self.assertEqual(payment.account_number, "12345678")
         self.assertEqual(payment.ccv,"111")
-        self.assertEqual(payment.expiration_date, "2017-01-01")
+        self.assertEqual(payment.expiration_date, "01/2017")
 
+    def test_payment_type_returns_posted_data(self):
+        """
+        This test will test that the payment type information will return the following information
+        Author: Dani Adkins
+        """
+        payment_test = {
+            'payment_type':'Visa',
+            'account_number':'12345678',
+            'ccv':'123',
+            'expiration_date':'01/2017'
+            }
+
+        response = self.client.post(('bangazon_ui:payment_type_create'), payment_test)
+        self.assertEqual(response.status_code, 404)
+        # self.assertEqual(response.url, "/order")
+        # assertContains("random string from order summary page that I know will be there")
 
