5993e47f26365882b4be6e2a566115165491f063
Dani Adkins <danirenaeadkins12@gmail.com>
2017-02-27 11:40:51 -0600

    added test to close order and added close order view

---
+++ bangazon/bangazon_ui/tests/test_product_detail_view.py
@@ -0,0 +1,50 @@
+import sys
+sys.path.append("../")
+from django.test import TestCase
+from django.contrib.auth.models import User
+from bangazon_ui.models.customer_model import Customer
+from bangazon_ui.models.product_model import Product
+from bangazon_ui.models.product_type_model import ProductType
+from bangazon_ui.views.product_detail_view import ProductDetailView
+from django.urls import reverse
+
+
+
+class TestProductDetailView(TestCase):
+    """
+    This is the test class for the Product Detail View
+
+    Author: Ben Marks, Ludicrous Ducks
+    """
+
+    def test_product_detail_view_should_return_product_details(self):
+        """
+        Method to ensure that the detail view is sending the correct product object to the template.
+        """
+        test_user = User.objects.create_user(
+            first_name="Dani",
+            last_name="Dani",
+            username="danidani"
+            )
+
+        test_customer = Customer.objects.create(
+            user=test_user,
+            address="3040 NSS Drive",
+            city="Nashville",
+            state="TN",
+            zip_code="12345",
+            phone="123456789"
+            )
+
+        test_product_type = ProductType.objects.create(label="Toys")
+
+        test_product = Product.objects.create(
+            name="lego",
+            price=100,
+            description="ultimate lego set",
+            quantity=2,
+            customer=test_customer,
+            product_type=test_product_type)
+
+        response = self.client.get(reverse('bangazon_ui:product_detail', args=([test_product.pk])))
+        self.assertEqual(response.context['product'].name, 'lego')
