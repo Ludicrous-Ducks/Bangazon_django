8f64785af2ae757dce6dd9fa2bd2e664bc81cda0
Ben Marks <onodric@yahoo.com>
2017-03-02 11:21:12 -0600

    finished navbar magic counter and welcome message

--- bangazon/bangazon_ui/templatetags/navbar_extras.py
+++ bangazon/bangazon_ui/templatetags/navbar_extras.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
 from bangazon_ui.models.customer_model import Customer
 from bangazon_ui.models.order_model import Order
 from bangazon_ui.models.product_model import Product
-from django.views.generic.base import TemplateView
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, login, authenticate
 from django import template
@@ -11,26 +8,38 @@ from django import template
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def welcome_and_cart(context):
+def welcome(context):
     """
-    Method to create a custom tag based on the current user context
+    Method to create a custom welcome tag based on the current user context
 
     Author: Ben Marks, Ludicrous Ducks
     """
-
-    user = context.user
+    user = context['user']
     if user.is_authenticated():
-        customer = Customer.objects.get(user = self.request.user.pk)
+        customer = Customer.objects.get(user = user.pk)
+        message = "Welcome, {} {}".format(customer.user.first_name, customer.user.last_name)
+    else:
+        message = ""
+
+    return message
+
+@register.simple_tag(takes_context=True)
+def cart(context):
+    """
+    Method to create a custom cart total tag based on the current user context
+
+    Author: Ben Marks, Ludicrous Ducks
+    """
+    user = context['user']
+    if user.is_authenticated():
+        customer = Customer.objects.get(user = user.pk)
         try:
             order = Order.objects.get_or_create(customer=customer, completed = 0)
             product_list = order[0].product.all()
         except:
             product_list = []
-        message = "Welcome, {} {}".format(customer.user.first_name, customer.user.last_name)
     else:
-        message = ""
         product_list = []
 
     product_count = len(product_list)
-    return { 'message': message, 'product_count': product_count }
-
+    return product_count
