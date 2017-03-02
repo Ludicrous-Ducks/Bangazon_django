cafdf4dde60e758f0559ba55b34f94c6b31ef3ff
Ben Marks <onodric@yahoo.com>
2017-03-02 09:56:36 -0600

    testing phase

--- bangazon/bangazon_ui/templatetags/navbar_extras.py
+++ bangazon/bangazon_ui/templatetags/navbar_extras.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from bangazon_ui.models import order_model
-from bangazon_ui.models import payment_type_model
+from bangazon_ui.models.customer_model import Customer
+from bangazon_ui.models.order_model import Order
+from bangazon_ui.models.product_model import Product
 from django.views.generic.base import TemplateView
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, login, authenticate
-from bangazon_ui.models import customer_model
-from django.db.models import F, Sum, FloatField
 from django import template
 
 register = template.Library()
@@ -21,17 +20,17 @@ def welcome_and_cart(context):
 
     user = context.user
     if user.is_authenticated():
-        customer = customer_model.Customer.objects.get(user = self.request.user.pk)
+        customer = Customer.objects.get(user = self.request.user.pk)
         try:
-            order = order_model.Order.objects.get_or_create(customer=customer, completed = 0)
+            order = Order.objects.get_or_create(customer=customer, completed = 0)
             product_list = order[0].product.all()
         except:
             product_list = []
-        product_count = len(product_list)
         message = "Welcome, {} {}".format(customer.user.first_name, customer.user.last_name)
     else:
         message = ""
         product_list = []
 
+    product_count = len(product_list)
     return { 'message': message, 'product_count': product_count }
 
